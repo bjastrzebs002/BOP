9831290a5f9106170da127d5f4733fb7fa4d7f79
Filip Katulski <filipkatulski@gmail.com>
2019-11-05 18:28:50 +0100

    Closing everything in Class type for testing purposes.

--- src/bop/PrepareData.py
+++ src/bop/PrepareData.py
@@ -1,44 +1,48 @@
 from datetime import datetime
 from datetime import timedelta
 
-lista = [{"rates":{"2018-09-01":{"USD":0.87},"2018-08-01":{"USD":0.85},"2018-07-30":{"USD":0.84},"2018-09-05":{"USD":0.86}}}]
+lista = [{"rates":{"2018-09-01":{"USD":0.87},"2018-08-01":{"USD":0.85},"2018-07-30":{"USD":0.84},"2018-09-05":{"USD":0.86}},{"2018-09-01":{"USD":0.87},"2018-08-01":{"USD":0.85},"2018-07-30":{"USD":0.84},"2018-09-05":{"USD":0.86}}]}]
 
 
-def prepare(unsorted_list):
-    r = unsorted_list[0]['rates']
-    dates_n_rates = []
-    for key, val in r.items():
-        temp_tuple = tuple((datetime.strptime(key, '%Y-%m-%d').date(), val['USD']))
-        dates_n_rates.append(temp_tuple)
-    sort_d(dates_n_rates)
-    add_missing_d(dates_n_rates)
-    return dates_n_rates
+class PrepareData:
+    def __init__(self, unsorted_list):
+        self.unsorted_list = unsorted_list
+        self.all_dates = []
+        self.results = []
+
+    def prepare(self):
+        for x in self.unsorted_list:
+            r = x['rates']
+            dates_n_rates = []
+            for key, val in r.items():
+                temp_tuple = tuple((datetime.strptime(key, '%Y-%m-%d').date(), val['USD']))
+                dates_n_rates.append(temp_tuple)
+            self.sort_d(dates_n_rates)
+            self.add_missing_d(dates_n_rates)
+            self.all_dates.append(dates_n_rates)
+        return self.all_dates, self.results
+
+    @staticmethod
+    def sort_d(dates_n_rates):
+        dates_n_rates.sort(key=lambda date: date[0])
+
+    def add_missing_d(self, dates_n_rates):
+        try:
+            for x in range(len(dates_n_rates)):
+                delta = dates_n_rates[x+1][0] - dates_n_rates[x][0]
+                if delta > timedelta(days=1):
+                    temp_date = dates_n_rates[x][0] + timedelta(days=1)
+                    new_data = (temp_date, dates_n_rates[x][1])
+                    dates_n_rates.insert(x+1, new_data)
+                    self.add_missing_d(dates_n_rates)
+        except IndexError:
+            pass
+
+    def compare(self, dates_n_rates):
+        return self.results
 
 
-def sort_d(dates_n_rates):
-    dates_n_rates.sort(key=lambda date: date[0])
-
-
-def add_missing_d(dates_n_rates):
-    try:
-        for x in range(len(dates_n_rates)):
-            delta = dates_n_rates[x+1][0] - dates_n_rates[x][0]
-            if delta > timedelta(days=1):
-                temp_date = dates_n_rates[x][0] + timedelta(days=1)
-                new_data = (temp_date, dates_n_rates[x][1])
-                dates_n_rates.insert(x+1, new_data)
-                add_missing_d(dates_n_rates)
-    except IndexError:
-        pass
-
-
-def compare(dates_n_rates):
-    
-    pass
-
-
-a = prepare(lista)
-
-print(a)
+A = PrepareData(lista)
+print(A.prepare())
 
 
