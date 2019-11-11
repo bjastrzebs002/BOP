4eaeb0d138954058fd50e5e8aaced3ecc8d89073
Kubidon41 <Kubano3ak@gmail.com>
2019-11-11 16:55:42 +0100

    Add RequestHandler which return a list of directories

--- src/bop/RequestHandler.py
+++ src/bop/RequestHandler.py
@@ -2,6 +2,9 @@ import json
 import requests
 import datetime
 import random
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 
 
@@ -22,7 +25,8 @@ class RequestHandler:
         return json.loads(r.text)
 
     def get_data(self):
-        for i in range(2):
+        logging.info("Downloading data from API..")
+        for i in range(1000):
             random_date = (random.randint(-1000, 1000))
             start_date = (self.DATETIME + datetime.timedelta(days=random_date))
             end_date = (start_date + datetime.timedelta(days=100))
